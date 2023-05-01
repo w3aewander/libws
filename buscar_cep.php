90f7c6335f648f7f7555d4540b6605f497024ba2
w3aewander <wander.silva@gmail.com>
2023-05-01 01:17:06 -0300

    Adicionado a libcalc para teste

--- buscar_cep.php
+++ buscar_cep.php
@@ -41,23 +41,6 @@
 include __DIR__ . "/libs/libcep.php"; 
 include __DIR__ . "/libs/libws.php"; 
 
-$libcalc = "https://github.com/w3aewander/libws/blob/master/libs/libcalc.php?action=somar&num1=10&num2=5";
-
-function get_include_contents($filename) {
-    
-   if (is_file($filename)) {
-       ob_start();
-       include $filename;
-       return ob_get_clean();
-   }   
-   return false;
-}
-
-get_include_contents($libcalc);
-//get_include_contents($libcep);
-
-echo somar();
-
 if ( $_SERVER['REQUEST_METHOD'] == 'POST'){
     
     $cep = $_POST['cep'];
