6d07b99398c3ddac0eec582ca8a6c0eca7f79f8d
w3aewander <wander.silva@gmail.com>
2023-05-01 00:54:45 -0300

    Adicionado a lib para consultar cep

--- libs/libws.php
+++ libs/libws.php
@@ -191,3 +191,10 @@ function get_include_contents($filename) {
     }   
     return false;
 }
+
+/**
+ * Função somar para testar o uso da biblioteca...
+ */
+function somar($num1, $num2){
+    return $num1 + $num2;
+}
